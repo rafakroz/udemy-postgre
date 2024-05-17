df18f36768ae0a2d297ba1d0614830d3a4248d4c
Rafael Queiroz <rafakroz@gmail.com>
2024-05-17 10:35:40 -0300

    Script atualizado, até a aula 27 da seção 2

--- scripts.sql
+++ scripts.sql
@@ -594,4 +594,74 @@ insert into produto (idproduto, idfornecedor, nome, valor) values (5, 2, 'Placa 
 insert into produto (idproduto, idfornecedor, nome, valor) values (6, 3, 'Memória RAM', 100);
 insert into produto (idproduto, idfornecedor, nome, valor) values (7, 1, 'Gabinete', 35);
 
+
+
+-- SEÇÃO 2 - AULA 24: EXERCÍCIO ----------------------------------------------------
+
+-- Criação da tabela Pedidos
+
+create table pedido (
+	idpedido integer not null,
+	idcliente integer not null,
+	idtransportadora integer,
+	idvendedor integer not null,
+	data_pedido date not null,
+	valor numeric (10,2) not null,
+
+	constraint pk_pdd_idpedido primary key (idpedido),
+	constraint fk_pdd_idcliente foreign key (idcliente) references cliente (idcliente),
+	constraint fk_pdd_idtransportadora foreign key (idtransportadora) references transportadora (idtransportadora),
+	constraint fk_pdd_idvendedor foreign key (idvendedor) references vendedor (idvendedor)
+);
+
+update cliente set nome = 'Manoel' where idcliente = 1;
+
+-- Inserts na tabela Pedido
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (1, '2008-04-01', 1300, 1, 1, 1);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (2, '2008-04-01', 500, 1, 1, 1);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (3, '2008-04-02', 300, 11, 2, 5);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (4, '2008-04-05', 1000, 8, 1, 7);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (5, '2008-04-06', 200, 9, 2, 6);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (6, '2008-04-06', 1985, 10, 1, 6);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (7, '2008-04-06', 800, 3, 1, 7);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (8, '2008-04-06', 175, 3, null, 7);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (9, '2008-04-07', 1300, 12, null, 8);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (10, '2008-04-10', 200, 6, 1, 8);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (11, '2008-04-15', 300, 15, 2, 1);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (12, '2008-04-20', 500, 15, 2, 5);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (13, '2008-04-20', 350, 9, 1, 7);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (14, '2008-04-23', 300, 2, 1, 5);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+	values (15, '2008-04-25', 200, 11, null, 5);
+
+
 ------------------------------------------------
